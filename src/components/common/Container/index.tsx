fcce94bebc322f3599f0b3095488fa733068e46a
Hoang Cong Phane <hoang.phancong@hdwebsoft.dev>
2023-01-30 15:25:03 +0700

    :sparkles: Feat: implement container

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -1,7 +1,16 @@
+import clsx from 'clsx';
 import React from 'react';
 
-interface ContainerProps {}
+interface ContainerProps {
+  children?: React.ReactNode;
+  fullScreen?: boolean;
+  className?: string;
+}
 
-const Container: React.FC<ContainerProps> = () => <div className='container'>Component Container</div>;
+const Container: React.FC<ContainerProps> = ({ fullScreen, className, children }) => (
+  <section className={clsx('mx-auto px-4 xl:px-6 2xl:px-28 2xl:max-w-screen-2xl', fullScreen && 'px-0', className)}>
+    {children}
+  </section>
+);
 
 export default Container;
